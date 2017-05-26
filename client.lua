599697f8affb842f35a5e2bde15fb7813bb93296
Somepotato <someguynamedpie@gmail.com>
2017-05-25 23:03:56 -0500

    interior DMCA alligator, I drive a Oggs now working and streaming!

--- client.lua
+++ client.lua
@@ -1,17 +1,16 @@
 package.path = package.path .. ';./modules/?.lua;./modules/?/init.lua'
-
+--require'log'.level = 'trace'
 local autoreload = require("autoreload")
 --local terminal = require("terminal")
 local concommand = require("concommand")
 local mumble = require("lumble")
-
 local socket = require("socket")
 
 require("scripts")
-
+local last = os.time()
 while true do
 	mumble.update()
-	socket.sleep(0.02)
+	socket.sleep(0.005)
 end
 
 --[[local function main()
